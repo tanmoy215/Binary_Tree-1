3e61b4029c93d69a1831c52f94e11adc65beb5f2
Tanmoy <majitanmoy14@gmail.com>
2024-09-19 10:51:17 +0530

    sep 19

--- practics.cpp
+++ practics.cpp
@@ -1,30 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Node{
-    public:
-     int data;
-     Node* left;
-     Node* right;
-     Node(int data){
-        this->data = data;
-        left =NULL;
-        right = NULL;
-     }
+  public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+       this->data = data;
+       right = left = nullptr;
+    }
 };
+void display(Node* root){
+    if(root==nullptr) return;
+    cout<<root->data<<" ";
+    display(root->left);
+    display(root->right);
+}
 int main(){
-   Node* root = new Node(1);
-   Node* a = new Node(2);
-   Node* b = new Node(3);
-   Node* c = new Node(4);
-   Node* d = new Node(5);
-   Node* e = new Node(6);
-   Node* f = new Node(8);
-   root->left = a;
-   root->right = b;
-   a->left = c;
-   a->right = d;
-   b->left = e;
-   b->right = f;
-   
+   Node* root = new Node(10);
+   Node* a = new Node(20);
+   Node* b = new Node(90);
+   Node* c = new Node(38);
+   Node* d = new Node(348);
+   Node* e = new Node(78);
+   Node* f = new Node(65); 
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    b->left = e;
+    b->right = f;
+    display(root);
    return 0;
 }
